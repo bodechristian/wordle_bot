b1019c52ce4d29971b1df4e736dd67bdbe3f6c89
Christian Bode <christianbode97@web.de>
2022-02-06 16:35:18 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import pickle
+
+from collections import Counter
+from util import *
+
+
+# initialize stuff
+nb_letters = 5
+with open(f"{nb_letters}-letter-combinations.pkl", "rb") as f:
+    all_words = pickle.load(f)
+wordlist = all_words.copy()
+
+frequency_table = {i: Counter() for i in range(nb_letters)}
+for word in all_words:
+    for i, letter in enumerate(word):
+        frequency_table[i][letter] += 1
+
+letters_correct = {}
+letters_contained = defaultdict(list)
+letters_incorrect = ""
+
+# wait a bit and then change focus to last window
+time.sleep(1)
+alt_tab()
+time.sleep(1)
+
+# do the 6 guesses
+for nb_guess in range(6):
+    print(f"Guess {nb_guess+1}:")
+    wordlist = update_wordlist(wordlist, letters_correct, letters_contained, letters_incorrect)
+    guess = solve(wordlist, all_words, frequency_table, letters_correct, letters_contained, letters_incorrect)
+    print(f"Guessing {guess}")
+    enter_guess(guess)
+    letters_correct, letters_contained, letters_incorrect = update_data(nb_guess, guess, letters_correct, letters_contained, letters_incorrect)
+    print()
---
+++ util.py
@@ -0,0 +1,179 @@
+import pyautogui
+import time
+import cv2
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from collections import defaultdict
+from PIL import Image
+from mss import mss
+
+
+def default_colour():
+    return "error"
+
+
+COLORS = defaultdict(default_colour, {
+    (83, 141, 78): "green",
+    (181, 159, 59): "yellow",
+    (58, 58, 60): "gray",
+})
+
+
+def alt_tab():
+    pyautogui.keyDown('alt')
+    time.sleep(.2)
+    pyautogui.press('tab')
+    time.sleep(.2)
+    pyautogui.keyUp('alt')
+
+
+def calc_score(_freqs, _word):
+    _res = 0
+    for i, letter in enumerate(_word):
+        _res += _freqs[i][letter]
+    return _res
+
+
+def guess_info(used_letters, _freqs, all_words):
+    """
+    go over list and calculate letter-frequency per slot
+    calculate score for each word
+    return word with max score - but ignore words with letters already used or double letters
+    @param: iterable of possible words
+    @param: iterable of letters that are contained/correct in the target word
+    """
+    max_score, max_word = 0, ""
+    for word in all_words:
+        # ignore words with already investigated letters or without 5 distinct letters
+        if any([_letter in word for _letter in used_letters]) or len(set(word)) < 5:
+            continue
+        score = calc_score(_freqs, word)
+        # save word if new max but
+        if score >= max_score:
+            max_score = score
+            max_word = word
+    return max_word
+
+
+def guess_solve(words, _freqs):
+    """
+    go over list and calculate letter-frequency per slot
+    calculate score for each word
+    return word with max score
+    @param: iterable of possible words
+    """
+    max_score, max_word = 0, ""
+    print(words)
+    for word in words:
+        score = calc_score(_freqs, word)
+        # save word if new max
+        if score >= max_score:
+            max_score = score
+            max_word = word
+    return max_word
+
+
+def update_wordlist(_cur_wordlist, _correct, _contained, _incorrect):
+    new_wordlist = set()
+
+    for word in _cur_wordlist:
+        keep = True
+
+        for letter, idx in _correct.items():
+            if not word[idx] == letter:
+                keep = False
+
+        for letter, idxs in _contained.items():
+            if not letter in word:
+                keep = False
+            for idx in idxs:
+                if word[idx] == letter:
+                    keep = False
+
+        for letter in _incorrect:
+            if letter in word:
+                keep = False
+
+        if keep:
+            new_wordlist.add(word)
+    return new_wordlist
+
+
+def solve(word_list, all_words, _freqs, _correct, _contained, _incorrect):
+    result = guess_info(list(_contained.keys())
+                        + list(_correct.keys())
+                        + list(_incorrect), _freqs, all_words)
+    return result if result != "" else guess_solve(word_list, _freqs)
+
+
+def enter_guess(_str):
+    # write guess and press enter, then wait for the websites animation
+    for _letter in _str:
+        pyautogui.press(_letter)
+        time.sleep(0.2)
+    pyautogui.press("enter")
+    time.sleep(3)
+
+
+def update_data(_nb_guess, _guess, _correct, _contained, _incorrect):
+    with mss() as sct:
+        # TODO: optimally have this be detected automatically
+        # can use sct.monitors[0] for all screens in 1 picture
+        # then find the grid
+        # then move it slightly so that top left corner is inside a cell to pick up colour
+        mon = sct.monitors[1]
+        grid_view = {"left": mon["left"] + 760, 'top': 330, 'width': 340, 'height': 420}
+        ss = sct.grab(grid_view)
+        # colour correction
+        img = Image.frombytes(
+            'RGB',
+            (ss.width, ss.height),
+            ss.rgb,
+        )
+        cell_height, cell_width = grid_view["height"] / 6, grid_view["width"] / 5
+
+        results = []
+        for idx_letter in range(5):
+            pxl = img.getpixel((int(idx_letter*cell_width), int(_nb_guess*cell_height)))
+            results.append(pxl)
+        results_colours = list(map(lambda x: COLORS[x], results))
+        print(results_colours)
+
+        for i, col in enumerate(results_colours):
+            if col == "green":
+                _correct[_guess[i]] = i
+            elif col == "yellow":
+                _contained[_guess[i]].append(i)
+            elif col == "gray":
+                _incorrect += _guess[i]
+
+        return _correct, _contained, _incorrect
+
+
+def draw_reading_points():
+    with mss() as sct:
+        mon = sct.monitors[1]
+        grid_view = {"left": mon["left"] + 765, 'top': 335, 'width': 340, 'height': 410}
+        ss = sct.grab(grid_view)
+        # colour correction
+        img = Image.frombytes(
+            'RGB',
+            (ss.width, ss.height),
+            ss.rgb,
+        )
+        cell_height, cell_width = grid_view["height"] / 6, grid_view["width"] / 5
+
+        plt.imshow(img)
+        plt.savefig("grid")
+
+        for idx_letter in range(5):
+            for j in range(6):
+                img.putpixel((int(idx_letter*cell_width), int(j*cell_height)), (255, 105, 180))
+        plt.imshow(img)
+        plt.show()
+
+
+if __name__ == "__main__":
+    draw_reading_points()
